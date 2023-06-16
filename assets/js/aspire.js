c38b8c9552e60796ff8580ee5860b858e48add0a
Abhishek88821 <127080867+Abhishek88821@users.noreply.github.com>
2023-06-16 11:57:29 +0530

    Add files via upload

--- assets/js/aspire.js
+++ assets/js/aspire.js
@@ -1,4 +1,14 @@
 $(document).ready(function(e) {
     
-    //
+    //menu start 
+    $(".mobile-bar").click(function(e) {
+        $('.sid-bar-div-box').toggleClass('open');
+        $(this).find('> i').toggleClass('fa-close');
+    });
+
+
+
+    //menu svg start 
+
+
 });
